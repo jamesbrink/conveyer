fbf86c0f40364e4fcfcbbe9c300d1a60d7d3d40b
James Brink <brink.james@gmail.com>
2018-06-14 23:00:17 -0700

    Added additional emulators

---
+++ docker-images/android/nexus5x_x86_google_apis_27/docker-assets/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Starting Android"
+export PATH=/opt/TurboVNC/bin:$PATH
+cleanup ()
+{
+  echo "Exiting"
+  vncserver -kill :1
+  pkill -15 emulator
+  exit $?
+}
+
+trap cleanup SIGTERM
+# Clean up any old vnc sessions
+rm -f /tmp/.X*-lock
+rm -rf /tmp/.X11-unix
+vncserver -desktop Android -alwaysshared
+jwm &
+emulator @Nexus_5X_API_27
+
+IP=`hostname -i`
+socat tcp-listen:5554,bind=$IP,fork tcp:127.0.0.1:5554 &
+socat tcp-listen:5555,bind=$IP,fork tcp:127.0.0.1:5555 &
+adb logcat &
+wait ${!}
